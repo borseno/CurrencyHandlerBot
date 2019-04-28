76bf6b530282f410bfbc039489b5b5e5aa5fbe1d
borseno <borseno2@gmail.com>
2019-04-28 10:36:47 +0300

    modify gitignore

--- CurrencyHandler/CurrencyHandler/Models/AppSettings.cs
+++ CurrencyHandler/CurrencyHandler/Models/AppSettings.cs
@@ -2,9 +2,9 @@
 {
     public static class AppSettings
     {
-        public const string Url = "https://YOUR_LINK:443/{0}";
+        public const string Url = "https://YOUR_LINK:443/{0}"; // 443 is the port for web hook
 
-        public const string Name = "YOUR_NAME";
+        public const string Name = "YOUR_NAME"; // bot name
 
         public const string Key = "YOUR_BOT_TOKEN";
     }
