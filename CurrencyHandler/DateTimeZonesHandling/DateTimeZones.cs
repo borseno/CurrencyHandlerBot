8e149fe9d1c59897095368d017e3e138304fa4e6
borseno <borseno2@gmail.com>
2019-05-18 16:38:33 +0300

    fixed datetimeinfo for linux

--- CurrencyHandler/DateTimeZonesHandling/DateTimeZones.cs
+++ CurrencyHandler/DateTimeZonesHandling/DateTimeZones.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using TimeZoneConverter;
 
 namespace DateTimeZonesHandling
 {
     public static class DateTimeZones
-    {
+    { 
+        private static TimeZoneInfo MoscowInfo { get; }
+
         public static DateTime NowMoscow
         {
             get
             {
                 var utc = DateTime.UtcNow;
                 var moscow = TimeZoneInfo
-                    .ConvertTime(utc, TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time"));
+                    .ConvertTime(utc, MoscowInfo);
 
                 return moscow;
+            }
+        }
+
+        static DateTimeZones()
+        {
+            const string WindowsMoscowTime = "Russian Standard Time";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                MoscowInfo = TimeZoneInfo.FindSystemTimeZoneById(WindowsMoscowTime);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                string linux = TZConvert.WindowsToIana(WindowsMoscowTime);
+
+                MoscowInfo = TimeZoneInfo.FindSystemTimeZoneById(linux);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                throw new NotImplementedException("Cannot get moscow time for OSX OS");
             }
         }
     }
