7c70a14bf91c567f555073830e6939c1ecb5a550
borseno <borseno2@gmail.com>
2019-05-28 11:04:00 +0300

    add synonyms for percent command

---
+++ CurrencyHandler/CurrencyHandler/Models/Commands/PercentsWithSynonimsCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CurrencyHandler.Models.Commands.Abstractions;
+using CurrencyHandler.Models.Database.Repositories;
+using CurrencyHandler.Models.InlineKeyboardHandlers.Abstractions;
+using Telegram.Bot.Types;
+
+namespace CurrencyHandler.Models.Commands
+{
+    public class PercentsWithSynonymsCommand : Command
+    {
+        private readonly ICommand mainCommand;
+
+        public PercentsWithSynonymsCommand(IKeyboards keyboards, ICurrenciesRepository repo) : base(keyboards, repo)
+        {
+            mainCommand = new PercentsCommand(keyboards, repo);
+        }
+
+        public override string Name => mainCommand.Name;
+
+        public override Task Execute(Message message)
+        {
+            return mainCommand.Execute(message);
+        }
+
+        public override bool Contains(string command)
+        {
+            const char Space = ' ';
+
+            var result = mainCommand.Contains(command);
+
+            var pureCommandName = command.Substring(0, command.IndexOf(Space));
+
+            return result ? result : mainCommand.Name.StartsWith(pureCommandName);
+        }
+    }
+}
